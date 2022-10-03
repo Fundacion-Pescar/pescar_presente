5e2ad2e126843dcddab4f6dcb18901c67b72ebda
Cristian Racedo <CristianDRacedo@hotmail.com>
2022-10-03 04:15:29 -0300

    Importacion de Modulo y Aplicacion de metodos

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,26 @@
+import { Alumno } from "./person_model.js";
 document.addEventListener('DOMContentLoaded', () => {
-    
+    // Instancia de la Clase (Objeto)
+    let alumno = new Alumno;
+    // Metodo de definicion Propiedades
+    alumno.set(
+        1,
+        'cristian', 
+        '35336446', 
+        'cristiandracedo@hotmail.com',
+        'Calle falsa 123',
+        'EducacionIT',
+        'Javascript Desarrollador Avanzado',
+        'lunes a viernes',
+        '10hs - 22hs'
+    );
+    // Metodo de Actualizacion (propiedad, valor)
+    alumno.put(
+        'name',
+        'Racedo Cristian Damian'
+    );
+    // Metodo de eliminacion
+    alumno.del('address');
+    // Resultado
+    console.log(alumno);
 }   );
